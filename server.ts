dbaed2bbb278704d64f2d2ed7d76e2e436695e56
anusha <arora.anusha@northeastern.edu>
2022-02-08 23:44:44 -0500

    atlas

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 
 //mongo connection
 
-mongoose.connect('mongodb://localhost:27017/tuiter').then(() => {
+mongoose.connect('mongodb+srv://anusha:fsePass@cluster0.lbwnn.mongodb.net/tuiter?retryWrites=true&w=majority').then(() => {
     console.log("Connected to DB");
 });
 const app = express();
