2206578e9893419d477bb2aeaaa68c4d5a9a50f1
anusha <arora.anusha@northeastern.edu>
2022-03-27 15:31:40 -0400

    updated cors

--- server.ts
+++ server.ts
@@ -31,8 +31,12 @@ mongoose.connect('mongodb+srv://anusha:fsePass@cluster0.lbwnn.mongodb.net/tuiter
 });
 
 const app = express();
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}))
 app.use(express.json());
-app.use(cors());
+
 
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
