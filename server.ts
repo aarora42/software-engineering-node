1344841cfef8b566b9f35376b204ef200c2cb317
anusha <arora.anusha@northeastern.edu>
2022-03-28 17:07:15 -0400

    local host

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ mongoose.connect('mongodb+srv://anusha:fsePass@cluster0.lbwnn.mongodb.net/tuiter
 
 const app = express();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
