46feb8cac02977af4a91a374d65fb581f63d5458
anusha <arora.anusha@northeastern.edu>
2022-03-28 16:55:10 -0400

    quick fix

--- server.ts
+++ server.ts
@@ -35,6 +35,15 @@ mongoose.connect('mongodb+srv://anusha:fsePass@cluster0.lbwnn.mongodb.net/tuiter
 });
 
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
