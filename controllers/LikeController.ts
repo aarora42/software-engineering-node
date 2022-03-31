9e7d9cdf62b850e83b4a671d0bdae6254dc670e9
anusha <arora.anusha@northeastern.edu>
2022-03-30 22:50:25 -0400

    likes

--- controllers/LikeController.ts
+++ controllers/LikeController.ts
@@ -141,7 +141,6 @@ export default class LikeController implements LikeControllerI {
         }
     }
 
-    //dislikes
     userTogglesTuitUnlikes = async (req: Request, res: Response) => {
         const likeDao = LikeController.likeDao;
         const tuitDao = LikeController.tuitDao;
